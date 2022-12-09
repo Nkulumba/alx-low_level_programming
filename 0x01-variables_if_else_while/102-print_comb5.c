66aacfef4e6d3f857cccd8bace96a2b4d029fd3d
root <kulumbanelson23@gmail.com>
2022-12-09 01:16:50 -0800

    task12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,25 +1,46 @@
 #include <stdio.h>
 
 /**
- *   * main - Prints a hexadecimal string
+ *   * main - Prints 3 combination of numbers
  *     *
  *       * Return: Always (Success)
  *         */
 int main(void)
 {
-		char c;
+	int c, i, k, j;
 
-			for (c = '0'; c <= '9'; c++)
-					{
-								putchar(c);
+	for (c = 48; c <= 57; c++)
+	  {
+	    for (i = 48; i <= 57; i++)
+		{
+		 for (k = 48; k <= 57; k++)
+		     {
+																								for (j = 48; j <= 57; j++)
+																								   {
+																								   if (((k + j) > (c + i) &&  k >= c) || c < k)
+																									{
+				   putchar(c);
+																									   putchar(i);
+																									   putchar(' ');
+																									    putchar(k);
+																								            putchar(j);
+
+	                  if (c + i + k + j == 227 && c == 57)
+																									{
+																								             break;
+																									}
+																									     else
+																									      {
+																									        putchar(',');
+																										putchar(' ');
+																							                      }
+																									}
+																								}
+																							}
+	}
 									}
 
-				for (c = 'a'; c <= 'f'; c++)
-						{
-									putchar(c);
-										}
-
-					putchar('\n');
-
-						return (0);
+		                putchar('\n');
+				return (0);
 }
+
