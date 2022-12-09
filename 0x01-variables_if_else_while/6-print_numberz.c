0222244d7ded6bd7d0999571ec3a82fdcd89f05b
root <kulumbanelson23@gmail.com>
2022-12-09 00:30:40 -0800

    task6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
 /**
- *  * main - Prints the alphabetic
- *   *
- *    * Return: Always (Success)
- *     */
+ *   * main - Prints the numbers since 0 to 9
+ *     *
+ *       * Return: Always (Success)
+ *         */
 int main(void)
 {
-		char c;
+	int n;
 
-	         for (c = 'a'; c <= 'z'; c++)
-		      putchar(c);
-		      putchar('\n');
-		  return (0);
+	for (n = '0'; n <= '9'; n++)
+	     {
+		putchar(n);
+	     }
+
+		putchar('\n');
+
+	return (0);
 }
+
