889030f570e4b51ab97eefdfa0e3966d78d9483e
root <kulumbanelson23@gmail.com>
2022-12-14 23:49:44 -0800

    task5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * more_numbers - prints the range 0-14 ten times.
+ * Return: nothing.
+ */
+void more_numbers(void)
+{
+int i, j;
+i = j = 0;
+while (i < 10)
+{
+while (j <= 14)
+{
+if (j >= 10)
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
+++j;
+}
+_char('\n');
+j = 0;
+i++;
+}
+}
