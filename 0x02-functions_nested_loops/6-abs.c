402715199012a45c23e35fbbc5f848ecec206b51
root <kulumbanelson23@gmail.com>
2022-12-15 12:39:49 -0800

    task6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,15 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * _abs - calculate the absolute value of the number from zero.
- * @i: the integer to be converted.
- * Return: absolute value of the input number.
+ * _abs - Computes the absolute value of an integer
+ * @n: The integer to be computed.
+ *
+ * return: The absolute value of the integer
  */
-int _abs(int i)
+int _abs(int n)
 {
-if (i < 0)
-i = i * -1;
-return (i);
+	if(n >= 0)
+		return (n);
+	else
+		return (-n);
 }
