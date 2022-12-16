5abebe8009bb11b5d28c055886bad22862661625
root <kulumbanelson23@gmail.com>
2022-12-16 01:46:22 -0800

    task1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,9 +8,7 @@
 int main(void)
 {
 	int i;
-
 	printf("infinite loop incoming :\n");
-
 	i = 0;
 	/*
 	 * while (i < 10)
@@ -19,6 +17,5 @@ int main(void)
 	 * }
 	 */
 	printf("infinite loop avoided!\\o/\n");
-
 	return (0);
 }
