75def38aa31bf1b6e286109253d2ff44e71c7ed2
root <kulumbanelson23@gmail.com>
2022-12-22 10:34:17 -0800

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,45 +1,35 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
  * print_number - prints an integer
- * @n:integer to be printed
- * Return: Always 0 (success)
+ * @n: input integer.
+ * Return: no return.
  */
 void print_number(int n)
 {
-	int power, neg, hold;
+	unsigned int m, d, count;
 
-	neg = 0;
-	power = 1;
-	hold = n;
 	if (n < 0)
 	{
-		_putchar('-');
-		neg = 1;
+		_putchar(45);
+		m = n * -1;
 	}
-	while (hold > 9 || hold < -9)
+	else
 	{
-		power *= 10;
-		hold /= 10;
+		m = n;
 	}
-	while (power > 0)
-	{
-		if (power > 9)
-		{
-			if (!neg)
-				_putchar((n / power % 10) + '0');
-			else
-		        	_putchar((n / power % 10) * -1 + '0');
 
-				power /= 10;
-		}
-		if (power == 1)
-		{
-			if (neg)
-				_putchar((n % 10) * -1 + '0');
-			else
-				_putchar(n % 10 + '0');
-			power = 0;
-		}
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar((m / count) % 10) + 48);
 	}
 }
