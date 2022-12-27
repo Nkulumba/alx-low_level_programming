dcc5f670c7fc77caa6868d74446725194828edca
root <kulumbanelson23@gmail.com>
2022-12-27 00:50:49 -0800

    task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,15 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-/**
- * File - main.h
- * Desk: Header contains prototypes for all functions
- */
-
-int _putchar(char c);
-
-
-
+int isupper(int c);
+int isdigit(int c);
+int mull(int a, int b);	
+void print_numbers(void);
+int _putchar(char);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void printsquare(int size);
+void print_triangle(int size);
+void print_number(int n);
 
 #endif
 	
