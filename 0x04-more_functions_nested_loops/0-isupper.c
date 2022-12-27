e9364b97ec1fbc5d5b8b5820b39577dde0d0b0d2
root <kulumbanelson23@gmail.com>
2022-12-27 00:34:53 -0800

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * _isupper - checks for uppercase.
  * @c: character to check.
@@ -5,15 +7,12 @@
  */
 int _isupper(int c)
 {
-char uppercase = 'A'
-int isupper = 0;
-for (; uppercase <= 'Z'; uppercase++)
-{
-if (c == uppercase)
-{
-isupper = 1;
-break;
-}
-}
-return (isupper);
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File - main.h
+ * Desk: Header contains prototypes for all functions
+ */
+
+int _putchar(char c);
+
+
+
+
+#endif
+	
