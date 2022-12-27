13a6dd1fcfbba2c85ac6d145ecb3d4c3b50749cb
root <kulumbanelson23@gmail.com>
2022-12-27 02:08:09 -0800

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,13 +1,29 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * print_line - draws a straight line (n) characters long.
- * @n: the number of underscores to print.
+ * print_line - draws a straight line in the terminal.
+ * @n: print int
+ * Return: Always 0.
  */
 void print_line(int n)
 {
+
 int i;
+
 if (n > 0)
+{
 for (i = 0; i < n; i++)
-_putchar('_');
-_putchar('\n');
 }
+_putchar (95);
+}
+
+_putchar ('\n');
+}
+
+else
+{
+_putchar ('\n');
+}
+
+}
+
+
