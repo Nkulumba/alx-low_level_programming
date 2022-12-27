a84aaff475053db1052a38d398112335666ad14c
root <kulumbanelson23@gmail.com>
2022-12-27 02:45:41 -0800

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,20 +1,30 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * print_square - draws a square using multiple terminal lines and #'s.
- * @n: the number of # to print in each line.
+ * print_square - prints a square
+ *@size: print size
+ * Return: Always 0.
  */
-void print_square(int n)
+
+void print_square(int size)
 {
-int i, j;
-if (n > 0)
+
+int i;
+int k;
+
+if (size > 0)
 {
-for (i = 0; i < n; i++)
+for (i = 0; i < size; i++)
 {
-for (j = 0; j < n; j++)
-_putchar('#');
+for (k = 0; k < size; k++)
+{
+_putchar(35);
+}
 _putchar('\n');
 }
 }
 else
 _putchar('\n');
 }
+}
+
+
