47f703835bcc6400812ace0555e0326239d631fc
root <kulumbanelson23@gmail.com>
2022-12-30 00:33:20 -0800

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,21 +5,18 @@
  * @n: number to start from
  * Return: 0 or 1
  */
-
 void print_to_98(int n)
 {
-
 while (n < 98)
 {
-        printf("%i, ", n);
-        n++;
+printf("%i, ", n);
+n++;
 }
 while (n > 98)
 {
-	printf("%i, ", n);
-	n--;
+printf("%i, ", n);
+n--;
 }
-
 printf("98");
 putchar('\n');
 }
