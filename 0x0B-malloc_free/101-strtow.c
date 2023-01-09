694c4df4cd8624325b04bd3fe18dd06cde522067
root <kulumbanelson23@gmail.com>
2023-01-09 06:01:17 -0800

    task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -33,7 +33,7 @@ int wordnos(char *s)
  * Return: pointer to an array of strings (Success)
  * or NULL (Error)
  */
-char *strtow(char *str)
+char **strtow(char *str)
 {
 	char **matrix, *tmp;
 	int i, k = 0, len = 0, words, c = 0, start, end;
