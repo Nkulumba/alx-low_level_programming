9e5b8f099e4b98ff3a8ff32b6415dd8b85b55a2d
root <kulumbanelson23@gmail.com>
2023-01-31 02:36:19 -0800

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,10 +2,10 @@
 /**
  * insert_nodeint_at_index - adds a new node to a linked list
  * at a given position
- * @head : pointer to the head of the list
- * @idx  : index of the list where the new node will be added
+ * @head: pointer to the head of the list
+ * @idx: index of the list where the new node will be added
  * (indices start at 0)
- * @n    : value for the new node to be added
+ * @n: value for the new node to be added
  * Return: pointer to the new node,
  * or NULL - if function fails
  */
